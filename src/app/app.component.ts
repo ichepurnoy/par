3ae0690a0406ef44e2f7b383f832ecc4aae08614
Ilya Chepurnoy <ichepurnoy@yahoo.com>
2020-11-13 00:59:25 +0200

    fix typo. Check if prettier is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {ToDo} from './app.model';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from './app.model';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    todos: ToDo[]
-
-    title = 'par';
+    todos: ToDo[];
 
     ngOnInit(): void {
         this.todos = [
             {
                 title: 'Title 1',
                 description: 'descr 1',
-                completed: false
+                completed: false,
             },
             {
                 title: 'Title 2',
                 description: 'descr 1',
-                completed: true
-            }
+                completed: true,
+            },
         ];
     }
 }
