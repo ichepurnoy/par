8b1db958912bf69fe560aa01ce400782be29a114
Ilya Chepurnoy <ichepurnoy@yahoo.com>
2020-11-13 01:51:48 +0200

    Basic separation of todos in 2 lists. MOving todos between lists by toggling a checkbox.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,27 @@ export class AppComponent implements OnInit {
             },
             {
                 title: 'Title 2',
-                description: 'descr 1',
+                description: 'descr 2',
+                completed: true,
+            },
+            {
+                title: 'Title 3',
+                description: 'descr 3',
+                completed: false,
+            },
+            {
+                title: 'Title 4',
+                description: 'descr 4',
+                completed: false,
+            },
+            {
+                title: 'Title 5',
+                description: 'descr 5',
                 completed: true,
             },
         ];
     }
+
+    getIncompleteTodos = todos => todos.filter(todo => !todo.completed);
+    getCompletedTodos = todos => todos.filter(todo => todo.completed);
 }
